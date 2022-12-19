b52d4dfe3f9f51f683d710f23eb5c1f83204b9bc
Nat! <nat@mulle-kybernetik.com>
2022-12-19 18:08:48 +0100

    fix homebrew formula

--- .mulle/etc/project/formula-info.sh
+++ .mulle/etc/project/formula-info.sh
@@ -3,13 +3,14 @@
 # formula operations.
 #
 PROJECT="mulle-craft"      # your project/repository uuid
-DESC="🚬 Build projects using mulle-make and mulle-sourcetre"
+DESC="🚬 Build projects using mulle-make and mulle-sourcetree"
 LANGUAGE="bash"                # c,cpp, objc, bash ...
 # NAME="${PROJECT}"        # formula filename without .rb extension
 
 DEPENDENCIES='${MULLE_SDE_TAP}mulle-sourcetree
 ${MULLE_SDE_TAP}mulle-make
 ${MULLE_SDE_TAP}mulle-dispense
+${MULLE_SDE_TAP}mulle-env
 '
 
-DEBIAN_DEPENDENCIES="mulle-sourcetree, mulle-make, mulle-dispense"
+DEBIAN_DEPENDENCIES="mulle-sourcetree, mulle-make, mulle-dispense, mulle-env"
